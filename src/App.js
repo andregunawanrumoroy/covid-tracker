4827ea12d096ed723c3710d2ddf3006091ff984f
andregunawanrumoroy <s11810023@student.unklab.ac.id>
2020-12-05 21:49:19 +0700

    Halaman commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import { Global, Indonesia, Provinsi } from "./Halaman";
 
-function App() {
+
+
+const App = () => {
   return (
     <div>
-      <h1>pertemuan pertama</h1>
+      <div classname="Header">
+        <h1>Covid Tracker</h1>
+      </div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Global</Link>
+          </li>
+          <li>
+            <Link to="/Indonesia">Indonesia</Link>
+          </li>
+          <li>
+            <Link to="/Provinsi">Provinsi</Link>
+          </li>
+        </ul>
+      </nav>
+      <Router>
+        <Switch>
+          <Route path="/Global">
+            <Global />
+          </Route>
+          <Route path="/Indonesia">
+            <Indonesia />
+          </Route>
+          <Route path="/">
+            <Provinsi />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
-}
+};
 
 export default App;
