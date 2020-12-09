3c03fcb47008a9338e12c0287f438c95ba9d5710
andregunawanrumoroy <s11810023@student.unklab.ac.id>
2020-12-09 15:51:13 +0700

    Api_provinsi

--- src/Halaman/Provinsi/Api_provinsi.js
+++ src/Halaman/Provinsi/Api_provinsi.js
@@ -1,29 +1,47 @@
 import React, { useEffect, useState } from "react";
-import axios from "axios";
-import NumberFormat from "react-number-format";
+import axios from 'axios';
+import NumberFormat from 'react-number-format'
 
-export default function Api_indonesia() {
-    const [dataPro, setDataList] = useState([]);
-    
-    useEffect (() => {
-        axios
-        .get("https://indonesia-covid-19.mathdro.id/api/provinsi")
-        .then(response => {
-            setDataList(response.data.data)
-        })
-        .catch (err => {
-            console.log(err)
-        })
+    export default function CardIndo(){
+        const [dataPro, provinsiList] = useState([]);
+
+   
+        useEffect(() => {
+            axios
+            .get("https://indonesia-covid-19.mathdro.id/api/provinsi")
+            .then(response => {
+                provinsiList(response.data.data)
+            })
+            .catch(err => {
+        console.log(err)
+    })
     }, []);
 
-    console.log(dataPro)
+    console.log (dataPro)
     return (
-        <>
-            {dataPro.map((item, index) => {
-                return(
-                    <NumberFormat value={dataPro} thousandSeparator={true} displayType={'text'}/>
-                )
-            })}
-        </>
+
+    <center>
+    <table border="2">
+                <tr>
+                    <th><center>No.</center></th>
+                    <th><center>Provinsi</center></th>
+                    <th><center>Positif</center></th>
+                    <th><center>Sembuh</center></th>
+                    <th><center>Meninggal</center></th>
+         
+                </tr>
+                 {dataPro.map((item, index) => {
+                  return(
+                      <tr>
+                          <th scope="row" key={item.fid}><center>{index + 1}.</center></th>
+                         <th> <center>{item.provinsi}</center></th>
+                          <th><center><NumberFormat value={item.kasusPosi} thousandSeparator={true} displayType={'text'}/></center></th>
+                          <th><center><NumberFormat value={item.kasusSemb} thousandSeparator={true} displayType={'text'}/></center></th>
+                         <th><center><NumberFormat value={item.kasusMeni} thousandSeparator={true} displayType={'text'}/></center></th>
+                       </tr>
+                 )
+             })}
+           </table>
+           </center>
     )
 }
